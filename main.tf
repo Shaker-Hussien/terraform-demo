f132373fac5aad12f9f2e314fffb3d9243c3b406
ShakerHussien <shakerhussiencmp@gmail.com>
2021-08-22 07:13:12 +0200

    edit entry script file

--- main.tf
+++ main.tf
@@ -100,14 +100,8 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
     
-    # user_data = file("entry-script.sh")
-    user_data = <<EOF
-                    #!/bin/bash
-                    sudo yum update -y && sudo yum install -y docker
-                    sudo systemctl start docker
-                    sudo usermod -aG docker ec2-user
-                    sudo docker run -p 8080:80 nginx
-                EOF
+    user_data = file("entry-script.sh")
+
     tags = {
         Name = "${var.env_prefix}-server"
     }
